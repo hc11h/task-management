3b30a34f44eaf0d09c69efbd1106e763683bf4cc
hc11h <chaniyara.harsh26@gmail.com>
2026-01-08 11:29:34 +0530

    prisma configs

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global()
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService], // Export so other modules can inject it
+})
+export class PrismaModule {}
